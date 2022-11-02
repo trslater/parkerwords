b24c271bc5a6f979640cc78054ac6c634a92b9cd
Tristan Slater <hello@trslater.ca>
2022-11-02 00:56:04 -0700

    Remove repeat letters in words

--- src/parkerwords.c
+++ src/parkerwords.c
@@ -21,22 +21,22 @@ int main(int argc, char const *argv[])
     char c;
     int i = 0;      // Line counter
     int j = 0;      // Char counter
-    // int valid = 1;
+    int valid = 1;
 
     while ((c = fgetc(words_file)) != EOF)
     {
-        // for (int k = 0; k < j; ++k) {
-        //     if (word[k] == c && j < WORD_LEN) {
-        //         valid = 0;
-        //         break;
-        //     }
-        // }
+        for (int k = 0; k < j; ++k) {
+            if (word[k] == c && j < WORD_LEN) {
+                valid = 0;
+                break;
+            }
+        }
 
         // If at end of line
         if (c == '\n')
         {
             // If end of line is at end of desired word length
-            if (j == WORD_LEN) {
+            if (j == WORD_LEN && valid) {
                 // printf("Max words: %lld\n", max_words);
                 if (num_words == max_words) {
                     max_words *= 2;
@@ -51,7 +51,7 @@ int main(int argc, char const *argv[])
          
             ++i;        // Advance line
             j = 0;      // Reset read head
-            // valid = 1;
+            valid = 1;
         }
         else {
             // Part of valid word
@@ -63,7 +63,7 @@ int main(int argc, char const *argv[])
     fclose(words_file);
 
 
-    for (int l = 0; l < 10; ++l) {
+    for (int l = 0; l < 10000; ++l) {
         printf("%s\n", words[l]);
     }
 
